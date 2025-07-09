eec2d043802ddaf6dadd4868a975b33bca245763
Paul Krivenko <ukr.empresario@gmail.com>
2025-07-09 13:03:50 -0700

    Update Vite configuration to include legacy support, top-level await plugin, and enhanced server CORS settings. Adjust build and dependency optimization targets for improved compatibility.

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,34 @@
 import { sveltekit } from '@sveltejs/kit/vite'
+import legacy from '@vitejs/plugin-legacy'
 import UnoCSS from 'unocss/vite'
 import { defineConfig } from 'vite'
+import topLevelAwait from 'vite-plugin-top-level-await'
 
 export default defineConfig({
-  plugins: [UnoCSS(), sveltekit()],
+  plugins: [
+    UnoCSS(),
+    topLevelAwait(),
+    sveltekit(),
+    // @ts-ignore
+    legacy({
+      targets: ['defaults', 'safari >= 13'],
+      additionalLegacyPolyfills: ['regenerator-runtime/runtime'],
+    }),
+  ],
+  server: {
+    cors: {
+      origin: 'https://time.tc.io',
+      credentials: true,
+      methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+      allowedHeaders: ['Content-Type', 'Authorization'],
+    },
+  },
+  build: {
+    target: ['es2017', 'edge88', 'firefox78', 'chrome87', 'safari13'],
+  },
+  optimizeDeps: {
+    esbuildOptions: {
+      target: 'es2017',
+    },
+  },
 })
